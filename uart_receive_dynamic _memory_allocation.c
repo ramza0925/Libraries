f3d4e5e7753688e2eb45205cca36f132a14cbc54
Emrah Duatepe <duatepee@gmail.com>
2018-05-24 20:10:21 +0300

    Add files via upload

---
+++ uart_receive_dynamic _memory_allocation.c
@@ -0,0 +1,191 @@
+#include "main.h"
+#include "stm32f1xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "stdlib.h"
+
+void SystemClock_Config(void);
+static void MX_NVIC_Init(void);
+void Clear_Buffer(void);
+
+
+
+	uint8_t rx_data=0;
+	uint8_t *rx_buffer;
+	uint8_t rx_index=0;
+	uint8_t  dataReady=0;
+	uint8_t length=0;
+	
+
+	
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+
+  MX_NVIC_Init();
+
+	
+	rx_buffer =	(uint8_t*)calloc(1,sizeof(uint8_t));
+	HAL_UART_Receive_IT(&huart1,&rx_data,1);
+	
+	
+  while (1)
+  {
+		Clear_Buffer();
+			
+  }
+
+}
+//****************************************************************************//
+void Clear_Buffer(void)
+{	
+		if(dataReady)
+		{
+			dataReady = 0;
+			for(int i=0;i<length;i++) rx_buffer[i] = NULL;
+			free(rx_buffer);
+			rx_buffer =	(uint8_t*)calloc(1,sizeof(uint8_t));
+		}
+}
+//Bu interrupt fonksiyonuna ait prototip zaten oldugundan ayrica tanimlamaya gerek yoktur
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance == USART1)
+	{
+		HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_13);	
+		
+	  if(rx_data != 13)	
+		{	
+			rx_buffer[rx_index++] = rx_data;
+			rx_buffer = (uint8_t*)realloc(rx_buffer,sizeof(uint8_t));
+		}
+		
+		else	
+		{
+			length = strlen(rx_buffer);
+			//Gönder
+			HAL_UART_Transmit(&huart1,rx_buffer,length,1000);
+			
+			rx_index = 0;
+			dataReady = 1;
+			
+		}
+			
+		
+		HAL_UART_Receive_IT(&huart1,&rx_data,1);
+	}
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/** NVIC Configuration
+*/
+static void MX_NVIC_Init(void)
+{
+  /* USART1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART1_IRQn);
+  /* USART2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(USART2_IRQn);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void _Error_Handler(char * file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
